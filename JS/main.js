b566ae24dad801a272d6e99163937c5c88371e29
Icedev <moromubo2004@gmail.com>
2026-02-21 15:53:38 +0100

    done

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+document
+document.title
+document.body
+document.body.children
+document.getElementById("board");
+document.title= "My Page"
+document.getElementById("p1-name").textContent="Sofia"
+document.getElementById("p1-name").append(" & friends")
+document.getElementById("p1-name").append(" Adeyemi")
+document.title.indexOf("T")
+document.title.includes("JavaScripT")
+document.querySelector("header h1").style.textTransform="upperCase"
